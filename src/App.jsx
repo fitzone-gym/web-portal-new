2763dd0b9f055983f5b804a5f39dfc4b2274f1a0
muralijasi <muralijasi@gmail.com>
2023-07-24 17:07:47 +0530

    Receptionist files updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
 import { useState } from "react";
 import "./App.css";
-import { Login } from "./pages/Login/Login";
-import { Members } from "./pages/Manager/Members";
-import { Staffmembers } from "./pages/Manager/Staffmembers";
-import { Addtrainer } from "./pages/Manager/Addtrainer";
-import { Dashboard } from "./pages/Manager/Dashboard";
-import { Trainer } from "./pages/Manager/Trainer";
-import { Doctor } from "./pages/Manager/Doctor";
+// import { Login } from "./pages/Login/Login";
+// import { Members } from "./pages/Manager/Members";
+// import { Staffmembers } from "./pages/Manager/Staffmembers";
+// import { Addtrainer } from "./pages/Manager/Addtrainer";
+// import { Dashboard } from "./pages/Manager/Dashboard";
+// import { Trainer } from "./pages/Manager/Trainer";
+// import { Doctor } from "./pages/Manager/Doctor";
+// import {ReceptionistDashboard} from './pages/Receptionist/R_Dashboard';
 
+// import R_Dashboard from "./pages/Receptionist/R_Dashboard";
+
+import R_Header from './components/Receptionist/header';
+import  R_Dashboard  from "./pages/Receptionist/R_Dashboard";
+import R_Memeberlist from "./pages/Receptionist/R_Memeberlist";
 
 function App() {
-  const [text, setText] = useState("");
+  
 
   return (
     <>
       {/* <Login /> */}
-      {/* <Dashboard /> */}
+      {/* <Dashboard />  */}
       {/* <Members />  */}
       {/* <Trainer /> */}
       {/* <Doctor /> */}
        {/* <Staffmembers /> */}
-       <Addtrainer />
+       {/* <Addtrainer /> */}
+
+       {/* <ReceptionistDashboard /> */}
+       <R_Memeberlist/>
     </>
   );
 }
---
+++ src/pages/Receptionist/R_Dashboard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../../components/Receptionist/header';
+import Sidenav from '../../components/Receptionist/sidenav';
+import Dashboard from '../../components/Receptionist/Dashboard';
+
+
+function R_Dashboard() {
+  return (
+    <div>
+        <Header/>
+        <Sidenav/>
+        <Dashboard/>
+
+    </div>
+  )
+}
+
+export default R_Dashboard
+
+
+
+
