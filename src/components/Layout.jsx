36c1c6b1a958484fcc8a44f3d1aa44b651b8fe9a
Ruwandimayunika <RuwandiMayunika01@gmail.com>
2023-08-20 17:50:45 +0530

    layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Header from "../../components/Receptionist/header";
+import Sidenav from "../../components/Receptionist/sidenav";
+import "../../styles/Receptionist/contactUsSubmitions.css";
+
+function Layout() {
+
+  return (
+    <>
+      <div className="contactUsMessages">
+        <Header />
+        <Sidenav />
+      </div>
+    </>
+  );
+}
+
+export default Layout;
